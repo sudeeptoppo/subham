4a12e7e754b15c31cc4486560a05e10db4817538
Sudeep Toppo <sudeeptoppo27@email.com>
2025-12-10 08:14:20 +0530

    setup express and mongoose

---
+++ projectSchool/backend/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const port = 3000;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1); // stop app if database fails
+  }
+};
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
